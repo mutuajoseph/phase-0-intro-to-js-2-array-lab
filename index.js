937ed64116b032a5797503d144856c35c4d65b07
mutuajoseph <wambuajoseph756@gmail.com>
2022-03-04 09:02:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+const cats =  ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    console.log(cats)
+}
+
+destructivelyAppendCat('Ralph')
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+destructivelyPrependCat("Bob")
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+destructivelyRemoveFirstCat()
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+appendCat("Broom")
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+prependCat("Arnold")
+
+function removeLastCat() {
+    const [firstElement, secondElement, ...restArr] = cats
+    return [firstElement, secondElement]
+}
+
+removeLastCat()
+
+function removeFirstCat() {
+    const [firstElement, ...restArr] = cats
+    return restArr
+}
+
+console.log(removeLastCat())
